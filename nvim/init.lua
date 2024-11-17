85f9cdb9b980403f008207eb2de851c4f7ad2d7f
Kristoffer Davidsen <88896307+nesdivad@users.noreply.github.com>
2024-11-17 22:09:46 +0100

    update tmux

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,8 @@
 local vim = vim
 
-local Plug = vim.fn['plug#']
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 vim.tabstop = 4
 vim.shiftwidth = 4
 vim.opt.termguicolors = true
-
-vim.call('plug#begin')
-
-Plug('nvim-tree/nvim-tree.lua')
-
-vim.call('plug#end')
-
-require("nvim-tree").setup()
-
-require("keymap")
-
-require'lspconfig'.pyright.setup{}
