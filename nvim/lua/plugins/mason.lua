ac343a059a906cd599b3c0c4a79574ab501a762f
Kristoffer Davidsen <88896307+nesdivad@users.noreply.github.com>
2025-08-09 10:31:20 +0200

    clean up mason deps

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,19 +4,22 @@ return {
         opts = {}
     },
     {
+        "neovim/nvim-lspconfig",
+        opts = {},
+        dependencies = {
+            "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim"
+        },
+        config = function()
+        end
+    },
+    {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "csharp_ls" },
+            ensure_installed = { "csharp_ls", "basedpyright" },
         },
         dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            {   
-                "neovim/nvim-lspconfig", 
-                opts = {},
-                config = function()
-
-                end
-            }
+            "mason-org/mason.nvim",
         },
     },
 }
