67466819aaa083d95f558c7a041f93b558ee50c2
Kristoffer Davidsen <88896307+nesdivad@users.noreply.github.com>
2025-08-19 19:23:15 +0200

    add brew commands to shell script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,13 @@
 #!bin/bash
 
+# Run the following command to find top-level packages for brew: 'brew leaves'
+# And to insert the list directly into 'brew.txt': 'brew leaves > brew.txt'
+
 brewpath="$(find . -name 'brew.txt')"
 xargs brew install < $brewpath
+
+# Run the following command to find installed casks: 'brew list --cask'
+# Run the following command to find installed casks and insert list into 'casks.txt': 'brew list --cask > casks.txt'
 
 caskpath="$(find . -name 'casks.txt')"
 xargs brew install --cask < $caskpath
