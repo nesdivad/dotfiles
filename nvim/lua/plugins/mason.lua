0780e3c8b87b4cb687fb535c6f8b1d993e2f808a
Kristoffer Davidsen <88896307+nesdivad@users.noreply.github.com>
2025-08-22 16:45:18 +0200

    csharp extended lsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -11,6 +11,15 @@ return {
             "mason-org/mason-lspconfig.nvim"
         },
         config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.csharp_ls.setup{
+                cmd = { "csharp-ls" },
+                filetypes = { "cs" },
+                init_options = { AutomaticWorkspaceInit = true },
+                on_attach = function()
+                    vim.keymap.set("n", "<leader>ren", vim.lsp.buf.rename, {})
+                end,
+            }
         end
     },
     {
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,14 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "Decodetalkers/csharpls-extended-lsp.nvim",
+        config = function()
+            local csharpls = require("telescope").load_extension("csharpls_definition")
+            vim.keymap.set("n", "<leader>def", csharpls.csharpls_definition, {})
+        end
+    }
 }
