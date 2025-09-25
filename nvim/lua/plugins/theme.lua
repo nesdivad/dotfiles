87fdce21fdb6576e0e39c530b5bdc3bc711a00de
Kristoffer Davidsen <88896307+nesdivad@users.noreply.github.com>
2025-09-25 21:49:06 +0200

    nightfox theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             vim.g.everforest_background = "hard"
             vim.g.everforest_enable_italic = 0
-            vim.cmd.colorscheme("everforest")
         end
-    }
+    },
+    { "EdenEast/nightfox.nvim" }
 }
